87a3d757055268f6f83847fd4e7376ca9ba5f4e8
Paulo Eduardo Magaldi <pauloeduardomagaldi@hotmail.com>
2020-10-26 19:21:51 -0300

    estendendo a classe string com o método

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace ExtensionMethods217
     {
         static void Main(string[] args)
         {
+            /*
             //meu ano e mes de aniversario e horas de nascimento
             DateTime dt = new DateTime(1971, 08, 13, 22, 00, 00);
 
@@ -18,7 +19,13 @@ namespace ExtensionMethods217
             dt = new DateTime(2020, 10, 26, 11, 30, 00);
 
             Console.WriteLine(dt.ElapsedTime());
+            */
 
+            string mensagem = "Olá, estendendo a minha classe string, com o meu método";
+
+            Console.WriteLine(mensagem.Cut(10));
+
+            
         }
     }
 }
