7ae35e0b1aa6f04041bfd1f7ed3c5ff03b3d80aa
Paulo Eduardo Magaldi <pauloeduardomagaldi@hotmail.com>
2020-10-26 19:11:38 -0300

    Estendendo o método DATETIME num método criado por mim na minha classe para o datetime padrao

---
+++ Extensions/DateTimeExtensions.cs
@@ -0,0 +1,30 @@
+﻿//using System;
+
+using System.Globalization;
+
+//namespace ExtensionMethods217.Extensions
+namespace System
+{
+    public static class DateTimeExtensions
+    {
+        public static string ElapsedTime(this DateTime thisObj)
+        {
+            TimeSpan duration = DateTime.Now.Subtract(thisObj);
+
+            if(duration.TotalHours < 24.0 )
+            {
+
+                return duration.TotalHours.ToString("F1", CultureInfo.InvariantCulture) + " horas";
+
+            }
+            else
+            {
+                return duration.TotalDays.ToString("F1", CultureInfo.InvariantCulture) + " dias";
+
+            }
+
+            
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+//using ExtensionMethods217.Extensions;
+
+namespace ExtensionMethods217
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //meu ano e mes de aniversario e horas de nascimento
+            DateTime dt = new DateTime(1971, 08, 13, 22, 00, 00);
+
+
+            Console.WriteLine(dt.ElapsedTime());
+
+            //o dia de hoje e as horas agora sao (26 de outubro de 2020, 19h06min q estou fazendo o exercício
+            dt = new DateTime(2020, 10, 26, 11, 30, 00);
+
+            Console.WriteLine(dt.ElapsedTime());
+
+        }
+    }
+}
